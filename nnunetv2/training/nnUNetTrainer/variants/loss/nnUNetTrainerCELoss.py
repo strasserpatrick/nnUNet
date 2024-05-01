733c687edf7b1757764e4eab8e577907382fb27f
Patrick Strasser <strasserpatrick@outlook.de>
2024-05-01 15:43:09 +0200

    feat: refine kwargs in parameter

--- nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerCELoss.py
+++ nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerCELoss.py
@@ -16,7 +16,7 @@ class nnUNetTrainerCELoss(nnUNetTrainer):
         # this gives higher resolution outputs more weight in the loss
         if self.enable_deep_supervision:
             deep_supervision_scales = self._get_deep_supervision_scales()
-            weights = np.array([1 / (2**i) for i in range(len(deep_supervision_scales))])
+            weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
             weights[-1] = 0
 
             # we don't use the lowest 2 outputs. Normalize weights so that they sum to 1
@@ -28,14 +28,15 @@ class nnUNetTrainerCELoss(nnUNetTrainer):
 
 class nnUNetTrainerCELoss_5epochs(nnUNetTrainerCELoss):
     def __init__(
-        self,
-        plans: dict,
-        configuration: str,
-        fold: int,
-        dataset_json: dict,
-        unpack_dataset: bool = True,
-        device: torch.device = torch.device("cuda"),
+            self,
+            plans: dict,
+            configuration: str,
+            fold: int,
+            dataset_json: dict,
+            unpack_dataset: bool = True,
+            device: torch.device = torch.device("cuda"),
+            **kwargs
     ):
         """used for debugging plans etc"""
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device, **kwargs)
         self.num_epochs = 5
