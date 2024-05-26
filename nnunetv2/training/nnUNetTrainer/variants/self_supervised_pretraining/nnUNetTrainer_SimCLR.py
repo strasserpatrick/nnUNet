7458a18cc8c1cb711bd4adb561744735014213a2
Patrick Strasser <patrick.strasser@stud-mail.uni-wuerzburg.de>
2024-05-26 15:55:05 +0000

    chore: mimic nnunet data augmentation

--- nnunetv2/training/nnUNetTrainer/variants/self_supervised_pretraining/nnUNetTrainer_SimCLR.py
+++ nnunetv2/training/nnUNetTrainer/variants/self_supervised_pretraining/nnUNetTrainer_SimCLR.py
@@ -8,11 +8,51 @@ from batchgenerators.transforms.noise_transforms import (
     GaussianNoiseTransform,
     GaussianBlurTransform,
 )
-from batchgenerators.transforms.crop_and_pad_transforms import RandomCropTransform
 from batchgenerators.transforms.utility_transforms import NumpyToTensor
-from batchgenerators.transforms.spatial_transforms import MirrorTransform, SpatialTransform, ResizeTransform
+from batchgenerators.transforms.spatial_transforms import (
+    MirrorTransform,
+    SpatialTransform,
+)
+from batchgenerators.transforms.abstract_transforms import AbstractTransform, Compose
+from batchgenerators.transforms.color_transforms import (
+    BrightnessMultiplicativeTransform,
+    ContrastAugmentationTransform,
+    GammaTransform,
+)
+from batchgenerators.transforms.noise_transforms import (
+    GaussianNoiseTransform,
+    GaussianBlurTransform,
+)
+from batchgenerators.transforms.resample_transforms import (
+    SimulateLowResolutionTransform,
+)
+from batchgenerators.transforms.spatial_transforms import (
+    SpatialTransform,
+    MirrorTransform,
+)
+from batchgenerators.transforms.utility_transforms import (
+    RemoveLabelTransform,
+    RenameTransform,
+    NumpyToTensor,
+)
 from torch import autocast
 
+from nnunetv2.training.data_augmentation.custom_transforms.cascade_transforms import (
+    ApplyRandomBinaryOperatorTransform,
+    MoveSegAsOneHotToData,
+    RemoveRandomConnectedComponentFromOneHotEncodingTransform,
+)
+from nnunetv2.training.data_augmentation.custom_transforms.deep_supervision_donwsampling import (
+    DownsampleSegForDSTransform2,
+)
+from nnunetv2.training.data_augmentation.custom_transforms.masking import MaskTransform
+from nnunetv2.training.data_augmentation.custom_transforms.region_based_training import (
+    ConvertSegmentationToRegionsTransform,
+)
+from nnunetv2.training.data_augmentation.custom_transforms.transforms_for_dummy_2d import (
+    Convert2DTo3DTransform,
+    Convert3DTo2DTransform,
+)
 from nnunetv2.training.nnUNetTrainer.variants.self_supervised_pretraining.helper.ssl_base_trainer import (
     nnUNetBaseTrainer,
 )
@@ -39,7 +79,8 @@ class nnUNetTrainer_SimCLR(nnUNetBaseTrainer):
         "use_projection_layer": True,
         "latent_space_dim": 8096,
         "num_val_iterations_per_epoch": 0,
-        "num_epochs": 5000
+        "batch_size": 8,
+        "num_epochs": 5000,
     }
 
     def __init__(
@@ -130,8 +171,8 @@ class nnUNetTrainer_SimCLR(nnUNetBaseTrainer):
         deep_supervision_scales: Union[List, Tuple, None],
         mirror_axes: Tuple[int, ...],
         do_dummy_2d_data_aug: bool,
-        order_resampling_data: int = 1,
-        order_resampling_seg: int = 0,
+        order_resampling_data: int = 3,
+        order_resampling_seg: int = 1,
         border_val_seg: int = -1,
         use_mask_for_norm: List[bool] = None,
         is_cascaded: bool = False,
@@ -139,25 +180,93 @@ class nnUNetTrainer_SimCLR(nnUNetBaseTrainer):
         regions: List[Union[List[int], Tuple[int, ...], int]] = None,
         ignore_label: int = None,
     ) -> AbstractTransform:
+        tr_transforms = []
+        if do_dummy_2d_data_aug:
+            ignore_axes = (0,)
+            tr_transforms.append(Convert3DTo2DTransform())
+            patch_size_spatial = patch_size[1:]
+        else:
+            patch_size_spatial = patch_size
+            ignore_axes = None
 
-        crop_factor = 0.7
-        data_aug_patch_size = tuple(int(crop_factor * val) for val in patch_size)
-
-        ssl_transforms = [
-            RandomCropTransform(crop_size=data_aug_patch_size),
-            GaussianNoiseTransform(p_per_sample=0.4, p_per_channel=0.4),
-            GaussianBlurTransform(p_per_sample=0.4, p_per_channel=0.4),
-            MirrorTransform(p_per_sample=0.4),
+        tr_transforms.append(
             SpatialTransform(
-                patch_size=data_aug_patch_size,
-                angle_x=rotation_for_DA['x'], 
-                angle_y=rotation_for_DA['y'], 
-                angle_z=rotation_for_DA['z'],
-                p_rot_per_axis=1,
-                label_key=None,
-            ),
-            ResizeTransform(target_size=patch_size, label_key=None),
-            NumpyToTensor(["data"], "float"),
-        ]
+                patch_size_spatial,
+                patch_center_dist_from_border=None,
+                do_elastic_deform=False,
+                alpha=(0, 0),
+                sigma=(0, 0),
+                do_rotation=True,
+                angle_x=rotation_for_DA["x"],
+                angle_y=rotation_for_DA["y"],
+                angle_z=rotation_for_DA["z"],
+                p_rot_per_axis=1,  # todo experiment with this
+                do_scale=True,
+                scale=(0.7, 1.4),
+                border_mode_data="constant",
+                border_cval_data=0,
+                order_data=order_resampling_data,
+                border_mode_seg="constant",
+                border_cval_seg=border_val_seg,
+                order_seg=order_resampling_seg,
+                random_crop=False,  # random cropping is part of our dataloaders
+                p_el_per_sample=0,
+                p_scale_per_sample=0.2,
+                p_rot_per_sample=0.2,
+                independent_scale_for_each_axis=False,  # todo experiment with this
+            )
+        )
 
-        return ContrastiveLearningViewGenerator(base_transforms=Compose(ssl_transforms))
+        if do_dummy_2d_data_aug:
+            tr_transforms.append(Convert2DTo3DTransform())
+
+        tr_transforms.append(GaussianNoiseTransform(p_per_sample=0.1))
+        tr_transforms.append(
+            GaussianBlurTransform(
+                (0.5, 1.0),
+                different_sigma_per_channel=True,
+                p_per_sample=0.2,
+                p_per_channel=0.5,
+            )
+        )
+        tr_transforms.append(
+            BrightnessMultiplicativeTransform(
+                multiplier_range=(0.75, 1.25), p_per_sample=0.15
+            )
+        )
+        tr_transforms.append(ContrastAugmentationTransform(p_per_sample=0.15))
+        tr_transforms.append(
+            SimulateLowResolutionTransform(
+                zoom_range=(0.5, 1),
+                per_channel=True,
+                p_per_channel=0.5,
+                order_downsample=0,
+                order_upsample=3,
+                p_per_sample=0.25,
+                ignore_axes=ignore_axes,
+            )
+        )
+        tr_transforms.append(
+            GammaTransform((0.7, 1.5), True, True, retain_stats=True, p_per_sample=0.1)
+        )
+        tr_transforms.append(
+            GammaTransform((0.7, 1.5), False, True, retain_stats=True, p_per_sample=0.3)
+        )
+
+        if mirror_axes is not None and len(mirror_axes) > 0:
+            tr_transforms.append(MirrorTransform(mirror_axes))
+
+        if use_mask_for_norm is not None and any(use_mask_for_norm):
+            tr_transforms.append(
+                MaskTransform(
+                    [i for i in range(len(use_mask_for_norm)) if use_mask_for_norm[i]],
+                    mask_idx_in_seg=0,
+                    set_outside_to=0,
+                )
+            )
+
+        tr_transforms.append(RemoveLabelTransform(-1, 0))
+
+        tr_transforms.append(NumpyToTensor(["data"], "float"))
+
+        return ContrastiveLearningViewGenerator(base_transforms=Compose(tr_transforms))
