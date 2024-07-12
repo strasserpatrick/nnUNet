9d49d4ca9071fee0c9a9d9e621ec719cbd86c962
Patrick Strasser <strasserpatrick@outlook.de>
2024-07-12 17:46:43 +0200

    feat: adjust moco to torchio contrastive

--- nnunetv2/training/nnUNetTrainer/variants/self_supervised_pretraining/nnUNetTrainer_MoCo.py
+++ nnunetv2/training/nnUNetTrainer/variants/self_supervised_pretraining/nnUNetTrainer_MoCo.py
@@ -12,10 +12,9 @@ from batchgenerators.transforms.noise_transforms import (
     GaussianNoiseTransform,
     GaussianBlurTransform,
 )
-from batchgenerators.transforms.crop_and_pad_transforms import RandomCropTransform
 from batchgenerators.transforms.resample_transforms import SimulateLowResolutionTransform
-from batchgenerators.transforms.utility_transforms import NumpyToTensor, RemoveLabelTransform
 from batchgenerators.transforms.spatial_transforms import MirrorTransform, SpatialTransform
+from batchgenerators.transforms.utility_transforms import NumpyToTensor, RemoveLabelTransform
 from torch import autocast
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.optim.lr_scheduler import LambdaLR
@@ -308,96 +307,7 @@ class nnUNetTrainer_MoCo(nnUNetSSLBaseTrainer):
             regions: List[Union[List[int], Tuple[int, ...], int]] = None,
             ignore_label: int = None,
     ) -> AbstractTransform:
-        tr_transforms = []
-        if do_dummy_2d_data_aug:
-            ignore_axes = (0,)
-            tr_transforms.append(Convert3DTo2DTransform())
-            patch_size_spatial = patch_size[1:]
-        else:
-            patch_size_spatial = patch_size
-            ignore_axes = None
-
-        tr_transforms.append(
-            SpatialTransform(
-                patch_size_spatial,
-                patch_center_dist_from_border=None,
-                do_elastic_deform=False,
-                alpha=(0, 0),
-                sigma=(0, 0),
-                do_rotation=True,
-                angle_x=rotation_for_DA["x"],
-                angle_y=rotation_for_DA["y"],
-                angle_z=rotation_for_DA["z"],
-                p_rot_per_axis=1,  # todo experiment with this
-                do_scale=True,
-                scale=(0.7, 1.4),
-                border_mode_data="constant",
-                border_cval_data=0,
-                order_data=order_resampling_data,
-                border_mode_seg="constant",
-                border_cval_seg=border_val_seg,
-                order_seg=order_resampling_seg,
-                random_crop=False,  # random cropping is part of our dataloaders
-                p_el_per_sample=0,
-                p_scale_per_sample=0.2,
-                p_rot_per_sample=0.2,
-                independent_scale_for_each_axis=False,  # todo experiment with this
-            )
-        )
-
-        if do_dummy_2d_data_aug:
-            tr_transforms.append(Convert2DTo3DTransform())
-
-        tr_transforms.append(GaussianNoiseTransform(p_per_sample=0.1))
-        tr_transforms.append(
-            GaussianBlurTransform(
-                (0.5, 1.0),
-                different_sigma_per_channel=True,
-                p_per_sample=0.2,
-                p_per_channel=0.5,
-            )
-        )
-        tr_transforms.append(
-            BrightnessMultiplicativeTransform(
-                multiplier_range=(0.75, 1.25), p_per_sample=0.15
-            )
-        )
-        tr_transforms.append(ContrastAugmentationTransform(p_per_sample=0.15))
-        tr_transforms.append(
-            SimulateLowResolutionTransform(
-                zoom_range=(0.5, 1),
-                per_channel=True,
-                p_per_channel=0.5,
-                order_downsample=0,
-                order_upsample=3,
-                p_per_sample=0.25,
-                ignore_axes=ignore_axes,
-            )
-        )
-        tr_transforms.append(
-            GammaTransform((0.7, 1.5), True, True, retain_stats=True, p_per_sample=0.1)
-        )
-        tr_transforms.append(
-            GammaTransform((0.7, 1.5), False, True, retain_stats=True, p_per_sample=0.3)
-        )
-
-        if mirror_axes is not None and len(mirror_axes) > 0:
-            tr_transforms.append(MirrorTransform(mirror_axes))
-
-        if use_mask_for_norm is not None and any(use_mask_for_norm):
-            tr_transforms.append(
-                MaskTransform(
-                    [i for i in range(len(use_mask_for_norm)) if use_mask_for_norm[i]],
-                    mask_idx_in_seg=0,
-                    set_outside_to=0,
-                )
-            )
-
-        tr_transforms.append(RemoveLabelTransform(-1, 0))
-
-        tr_transforms.append(NumpyToTensor(["data"], "float"))
-
-        return ContrastiveLearningViewGenerator(base_transforms=Compose(tr_transforms))
+        return ContrastiveLearningViewGenerator(crop_size=patch_size)
 
     @torch.no_grad()
     def _dequeue_and_enqueue(self, keys):
