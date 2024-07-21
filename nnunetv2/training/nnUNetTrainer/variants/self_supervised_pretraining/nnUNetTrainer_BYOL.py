894be4973b6d013514d7768193389d5265fb49c0
Patrick Strasser <strasserpatrick@outlook.de>
2024-07-21 21:25:39 +0200

    chore: adam to 1e-4

--- nnunetv2/training/nnUNetTrainer/variants/self_supervised_pretraining/nnUNetTrainer_BYOL.py
+++ nnunetv2/training/nnUNetTrainer/variants/self_supervised_pretraining/nnUNetTrainer_BYOL.py
@@ -25,7 +25,7 @@ from nnunetv2.utilities.label_handling.label_handling import (
 class nnUNetTrainer_BYOL(nnUNetSSLBaseTrainer):
     DEFAULT_PARAMS: dict = {
         "num_epochs": 300,
-        "learning_rate": 1e-3,
+        "learning_rate": 1e-4,
         # "sgd_momentum": 0.9,
         "weight_decay": 1e-4,
         "hidden_dim": 256,
